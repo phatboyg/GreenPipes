f948c64c5daa5175b1296558aab1804ab8151988
Chris Patterson <chris@phatboyg.com>
2016-09-07 09:12:41 -0700

    Added visualization to the composed pipe

--- src/GreenPipes.Tests/Parent_Child_Pipes.cs
+++ src/GreenPipes.Tests/Parent_Child_Pipes.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 
 namespace GreenPipes.Tests
 {
+    using System;
+
+
     [TestFixture]
     public class Parent_Child_Pipes
     {
@@ -38,6 +41,42 @@ namespace GreenPipes.Tests
 
             Assert.That(count1, Is.EqualTo(1));
             Assert.That(count2, Is.EqualTo(1));
+
+
+            Console.WriteLine(pipe2.GetProbeResult().ToJsonString());
+        }
+
+        [Test]
+        public async Task Should_compose_pipes()
+        {
+            var count1 = 0;
+            var count2 = 0;
+
+            var pipe1 = Pipe.New<InitialContext>(xx =>
+            {
+                xx.UseExecute(p =>
+                {
+                    Interlocked.Increment(ref count1);
+                });
+            });
+
+            var pipe2 = Pipe.New<InitialContext>(x =>
+            {
+                x.UseFork(pipe1);
+
+                x.UseExecuteAsync(async payload =>
+                {
+                    Interlocked.Increment(ref count2);
+                });
+            });            
+
+            await pipe2.Send(new InitialContext()).ConfigureAwait(false);
+
+            Assert.That(count1, Is.EqualTo(1));
+            Assert.That(count2, Is.EqualTo(1));
+
+
+            Console.WriteLine(pipe2.GetProbeResult().ToJsonString());
         }
 
         class InitialContext :
