a5670aa733f9e5dc38634ff24d3c0727a78bd92c
Dru Sellers <dru@drusellers.com>
2016-09-20 17:29:19 -0500

    Add a concept of a Context Source

---
+++ src/GreenPipes.Tests/Source_Specs.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using GreenPipes.Payloads;
+using NUnit.Framework;
+using Timer = System.Timers.Timer;
+
+namespace GreenPipes.Tests
+{
+    public class Source_Specs
+    {
+        [Test]
+        public async Task X()
+        {
+            var ts = new CancellationTokenSource();
+            int i = 0;
+
+            var pipe = Pipe.New<FileContext>(cfg =>
+            {
+                cfg.UseExecute(cxt =>
+                {
+                    i++;
+                    Console.WriteLine(cxt.FilePath);
+                });
+            });
+
+            var source = new TestContextSource();
+
+            source.Fill(pipe, ts.Token);
+
+            await Task.Delay(1000);
+
+            ts.Cancel();
+
+            //TODO: not sure how to best test that the cancel was propagated.
+            int j = i;
+
+            await Task.Delay(1000);
+
+            Assert.That(ts.IsCancellationRequested, Is.True);
+            Assert.That(i, Is.GreaterThan(1));
+            Assert.That(j, Is.EqualTo(i));
+        }
+
+    }
+
+    public class TestContextSource : IContextSource<FileContext>
+    {
+        Timer _timer;
+
+        public async Task Fill(IPipe<FileContext> pipe, CancellationToken token = new CancellationToken())
+        {
+            _timer = new Timer(100);
+            _timer.Start();
+            _timer.Elapsed += (sender, args) =>
+            {
+                pipe.Send(new FileContext()
+                {
+                    FilePath = "A:"+DateTime.Now
+                });
+            };
+
+            while (!token.IsCancellationRequested)
+            {
+                if (token.IsCancellationRequested)
+                    break;
+
+                await Task.Delay(10, token);
+            }
+
+            _timer.Stop();
+
+        }
+    }
+    public class FileContextSource : IContextSource<FileContext>
+    {
+        public async Task Fill(IPipe<FileContext> pipe, CancellationToken token = new CancellationToken())
+        {
+            //I know its not the best, just using it as a demo
+            using (var fw = new FileSystemWatcher())
+            {
+                fw.Created += BuildHandler(pipe);
+
+                while (!token.IsCancellationRequested)
+                {
+                    //pause here so we don't thrash???
+                    await Task.Delay(100, token);
+                }
+            }
+        }
+
+        FileSystemEventHandler BuildHandler(IPipe<FileContext> pipe)
+        {
+            return (s, e) =>
+            {
+                var cxt = new FileContext();
+                cxt.FilePath = e.FullPath;
+                //not awaiting so i don't block
+                pipe.Send(cxt);
+            };
+        }
+    }
+
+    public class FileContext : BasePipeContext, PipeContext
+    {
+        public FileContext() : base(new PayloadCache())
+        {
+
+        }
+
+        public string FilePath { get; set; }
+    }
+
+
+}
---
+++ src/GreenPipes/IContextSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace GreenPipes
+{
+    public interface IContextSource<TContext> where TContext : class, PipeContext
+    {
+        Task Fill(IPipe<TContext> pipe, CancellationToken token = default(CancellationToken));
+    }
+}
