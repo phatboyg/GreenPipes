b98e62eca5cd7aa5ad73bbeb6b16bf89007e3d9e
Chris Patterson <chris@phatboyg.com>
2017-05-31 13:19:58 -0700

    Versioning and symbols created.

--- build.fsx
+++ build.fsx
@@ -5,13 +5,12 @@ open Fake.AssemblyInfoFile
 open Fake.Git.Information
 open Fake.SemVerHelper
 
-let buildOutputPath = FullName "./build_output"
 let buildArtifactPath = FullName "./build_artifacts"
 let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./GreenPipes.snk"
 
 let assemblyVersion = "1.0.0.0"
-let baseVersion = "1.0.9"
+let baseVersion = "1.0.10"
 
 let semVersion : SemVerInfo = parse baseVersion
 
@@ -39,14 +38,13 @@ let nugetVersion = (fun _ ->
 let InfoVersion = informationalVersion()
 let NuGetVersion = nugetVersion()
 
+let versionArgs = [ @"/p:Version=""" + NuGetVersion + @""""; @"/p:AssemblyVersion=""" + FileVersion + @""""; @"/p:FileVersion=""" + FileVersion + @""""; @"/p:InformationalVersion=""" + InfoVersion + @"""" ]
 
 printfn "Using version: %s" Version
 
 Target "Clean" (fun _ ->
-  ensureDirectory buildOutputPath
   ensureDirectory buildArtifactPath
 
-  CleanDir buildOutputPath
   CleanDir buildArtifactPath
 )
 
@@ -55,25 +53,18 @@ Target "RestorePackages" (fun _ ->
 )
 
 Target "Build" (fun _ ->
-
-  CreateCSharpAssemblyInfo @".\src\SolutionVersion.cs"
-    [ Attribute.Title "GreenPipes"
-      Attribute.Description "GreenPipes, a pipes and filters library for the Task Parallel Library"
-      Attribute.Product "GreenPipes"
-      Attribute.Version assemblyVersion
-      Attribute.FileVersion FileVersion
-      Attribute.InformationalVersion InfoVersion
-    ]
   DotNetCli.Build (fun p-> { p with Project = @".\src\GreenPipes"
                                     Configuration= "Release"
-                                    Output = buildArtifactPath})
+                                    Output = buildArtifactPath
+                                    AdditionalArgs = versionArgs })
 )
 
 Target "Package" (fun _ ->
   DotNetCli.Pack (fun p-> { p with 
                                 Project = @".\src\GreenPipes"
                                 Configuration= "Release"
-                                OutputPath= buildArtifactPath })
+                                OutputPath= buildArtifactPath
+                                AdditionalArgs = versionArgs @ [ @"--include-symbols"; @"--include-source" ] })
 )
 
 Target "Default" (fun _ ->
